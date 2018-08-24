565eccc7069fe30a58a8112bc351a0095fb9a693
EC2 Default User <ec2-user@ip-172-31-31-120.us-east-2.compute.internal>
2018-08-24 07:27:58 +0000

    portfolio

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails', '4.3.1'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.6.4'
 gem 'bootsnap', require: false
+gem 'friendly_id', '~> 5.2.0'
 
 group :development, :test do
   gem 'sqlite3',      '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :portfolios, except: [:show]
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio/show'
+  
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
+  
+  root to: 'pages#home'
 end
