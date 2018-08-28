b2fa9ed3894b28fe4e7f6870c1c3166cdedd9324
EC2 Default User <ec2-user@ip-172-31-31-120.us-east-2.compute.internal>
2018-08-28 15:54:42 +0000

    Finished Image Up

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['aws_access_key_id'],  # required
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],     # required
+    region:                'us-east-2',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
+    #host:                  's3.example.com',             # optional, defaults to nil
+    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'portfolio-tsubasa'                # required
+  config.fog_public     = false                                                 # optional, defaults to true
+  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+end
