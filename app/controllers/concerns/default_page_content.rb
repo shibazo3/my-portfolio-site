83e523a1f9b5e5eb1d218eda9549c7b21f498c5e
EC2 Default User <ec2-user@ip-172-31-31-120.us-east-2.compute.internal>
2018-09-01 09:37:44 +0000

    title changed

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
   
   def set_page_defaults
-    @page_title = "DevcampPortfolio | My Portfolio Website"
+    @page_title = "My Portfolio Website"
     @seo_keywords = "Tsubasa Shibata Portfolio"
   end
 end
