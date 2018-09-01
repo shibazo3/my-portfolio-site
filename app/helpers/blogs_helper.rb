92ed0c0e4a876997f80a7a0d3efe6ad274f9205b
EC2 Default User <ec2-user@ip-172-31-31-120.us-east-2.compute.internal>
2018-09-01 03:30:19 +0000

    blogshow9

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -9,19 +9,19 @@ module BlogsHelper
     end
   end
 
-  def markdown(text)
-    language ||= :textplain
-    coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
+  def markdown
+    language ||= :plaintext
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
     options = {
-      :fenced_code_blocks => true, #ここのoptionは任意で好きなようにカスタマイズしてok
-      :no_intra_emphasis  => true,
-      :autolink           => true,
-      :strikethrough      => true,
-      :lax_html_blocks    => true,
-      :superscript        => true
-     }
-     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-     markdown_to_html.render(text).html_safe
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
   end
 
   def blog_status_color blog
